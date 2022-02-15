cf9746a89bf7cbf4c5743bb41e326d8513c47c73
Koriome <koriome504@gmail.com>
2022-02-15 11:49:46 -0500

    Update bot.js
    my unsuccessful attempts to make this shit work
    
    im ditching my .env file for now cause it was causing issues. will fix at home

--- bot.js
+++ bot.js
@@ -1,19 +1,18 @@
 const tmi = require('tmi.js');
-require('dotenv').config();
 
-// Define configuration options
-const opts = {
-  identity: {
-    username: process.env.BOT_USERNAME,
-    password: process.env.OAUTH_TOKEN
+// connect to twitch irc
+const client = new tmi.Client({
+  options: { debug: true },
+  connection: {
+    secure: true,
+    reconnect: true
   },
-  channels: [
-    process.env.CHANNEL_NAME
-  ]
-};
-
-// Create a client with our options
-const client = new tmi.client(opts);
+  identity: {
+    username: 'straydavi',
+    password: 'oauth:fuckyou'
+  },
+  channels: ['koriome']
+});
 
 // Register our event handlers (defined below)
 client.on('message', onMessageHandler);
@@ -26,11 +25,9 @@ client.connect();
 function onMessageHandler (target, context, msg, self) {
   if (self) { return; } // Ignore messages from the bot
 
-  // Remove whitespace from chat message
-  const commandName = msg();
 
   // If the command is known, let's execute it
-  if (commandName.inclues(process.env.WORD)) {
+  if (msg.inclues('swag')) {
     client.say(target `has said the word. your time has come. @koriome`);
     console.log(`* THE WORD HATH BEEN SAID = ${commandName}`);
   } else {
@@ -42,3 +39,13 @@ function onMessageHandler (target, context, msg, self) {
 function onConnectedHandler (addr, port) {
   console.log(`* Connected to ${addr}:${port}`);
 }
+
+var fs = require('fs');
+var util = require('util');
+var log_file = fs.createWriteStream(__dirname + '/debug.log', {flags : 'w'});
+var log_stdout = process.stdout;
+
+console.log = function(d) { //
+  log_file.write(util.format(d) + '\n');
+  log_stdout.write(util.format(d) + '\n');
+};
